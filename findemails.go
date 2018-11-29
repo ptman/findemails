803f5fb19338c1f1d73e821f4c28375d0469d7e9
Paul Tötterman <paul.totterman@iki.fi>
2018-11-29 17:49:50 +0200

    Use HTML5 email regex for searching

--- findemails.go
+++ findemails.go
@@ -7,8 +7,9 @@ import "regexp"
 // TODO: more valid characters?
 // TODO: stricter limits on domain names?
 
+// https://www.w3.org/TR/html5/forms.html#valid-e-mail-address
 var emailRE = regexp.MustCompile(
-	`[-a-zA-Z0-9_\+\.]+@([a-zA-Z][-a-zA-Z0-9]*\.)+[a-zA-Z][-a-zA-Z0-9]+`)
+	"[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*")
 
 // FindEmails returns a slice of email addresses found in the input
 func FindEmails(src string) []string {
